b0b5863f5f86f6d1a9dc63793ccce73a4269b394
edwwwi <edwinjoy777@gmail.com>
2025-05-15 02:23:20 +0530

    star

--- PATTERN ABC.py
+++ PATTERN ABC.py
@@ -1,5 +1,5 @@
 
-for i in range(10,0,-1):
-    for j in range(1, i + 1):
-        print(chr(64 + j), end='')  
+for i in range(5,0,-1):
+    for j in range(i, 0,-1):
+        print(j, end='')  
     print()
